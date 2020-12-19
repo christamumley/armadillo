5461b87a1e300d8ba00ba00e62dc0054cd5a2719
Christa Mumley <mumley.c@husky.neu.edu>
2020-12-18 20:53:42 -0500

    set up libgdx proj

---
+++ desktop/src/com/armadillo/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.armadillo.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.armadillo.game.ArmadilloMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArmadilloMain(), config);
+	}
+}
